96d7595851302b3a116ec0380846c02058df2262
jadiel07 <jadiel174@hotmail.com>
2026-06-17 18:28:27 -0300

    Testando

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Api Funcionando"}
+@app.get("/chat")
+def chat(mensagem: str):
+
+    if "python" in mensagem.lower():
+        resposta = "Python é uma linguagem muito usada para backend, dados e IA."
+
+    elif "fastapi" in mensagem.lower():
+        resposta = "FastAPI é um framework moderno para criação de APIs."
+
+    else:
+        resposta = "Ainda estou aprendendo."
+
+    return {
+        "usuario": mensagem,
+        "ia": resposta
+    }
